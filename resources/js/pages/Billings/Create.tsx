4f637d372d79a5b9ed5a77731e009ce95f3b1eec
Jet777marshall <j.arabaca.533418@umindanao.edu.ph>
2026-05-21 08:01:58 +0800

    Update Create.tsx

--- resources/js/pages/Billings/Create.tsx
+++ resources/js/pages/Billings/Create.tsx
@@ -1,7 +1,10 @@
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head, Link } from '@inertiajs/react';
+import { Textarea } from '@/components/ui/textarea';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -15,8 +18,24 @@ export default function Index() {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Create Bill" />
-            <div>
-                 <Link href={route('billings.create')}>Create Bill</Link>
+            <div className='w-8/12 p-4'>
+                <form>
+                    <div className='gap-1.5'>
+                        <Label htmlFor="due_date">Due Date</Label>
+                        <Input
+                            type="date"
+                            id="due_date"
+                        />
+                    </div>
+                    <div className='gap-1.5'>
+                        <Label htmlFor="amount">Amount</Label>
+                        <Input placeholder='Amount'></Input>
+                    </div>
+                    <div className='gap-1.5'>
+                        <Label htmlFor="description">Description</Label>
+                        <Textarea placeholder='Description'></Textarea>
+                    </div>
+                </form>
             </div>
         </AppLayout>
     );
